1f86cffe0471c67d5a43721c67103b1d4ab414c5
Rajibbepary <rajib635356@gmail.com>
2025-04-17 13:28:36 +0600

    skill section add

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import { HelmetProvider } from 'react-helmet-async';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
    <HelmetProvider>
-   <div className='max-w-screen-xl mx-auto bg-[#111122]'>
+   <div className='max-w-screen-xl mx-auto bg-[#111122] font-Philosopher'>
    <RouterProvider router={router} />
    </div>
     </HelmetProvider>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from './Banner';
+import Skill from './Skill';
 
 const Home = () => {
     return (
         <div>
             <Banner/>
+            <Skill/>.
         </div>
     );
 };
